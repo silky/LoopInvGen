a89f62042a412194f0c23a71f28d98c58a3d5470
Saswat Padhi <padhi@cs.ucla.edu>
2019-01-11 10:45:18 +0000

    Original operator order

--- src/Logic.ml
+++ src/Logic.ml
@@ -7,19 +7,19 @@ type t = {
 }
 
 let all_supported =
-  let table = String.Table.create () ~size:2 in
-  let except (with_name : string) (component : Expr.component)
-      = not (String.equal component.name with_name)
+  let table = String.Table.create () ~size:2
    in List.iter ~f:(fun component -> String.Table.set table ~key:component.name ~data:component)
         [{
            name = "LIA" ;
-           components = Th_Bool.components @ Th_LIA.components ;
+           components = Th_LIA.components @ Th_Bool.components ;
            conflict_group_size_multiplier = 1
          } ; {
            name = "NIA" ;
-           components = Th_Bool.components
-                      @ (List.filter Th_LIA.components ~f:(except "lin-int-mult"))
-                      @ Th_NIA.components ;
+           components = (List.hd_exn Th_LIA.components)
+                     :: (List.hd_exn Th_NIA.components)
+                     :: ( (List.tl_exn Th_LIA.components)
+                        @ (List.tl_exn Th_NIA.components)
+                        @ Th_Bool.components);
            conflict_group_size_multiplier = 2
          }]
     ; table
--- src/Th_NIA.ml
+++ src/Th_NIA.ml
@@ -6,18 +6,16 @@ let pos_div x y = (x - (x % y)) / y
 
 let components = let (=/=) = (fun x y -> (not (Expr.equal x y))) in [
   {
-    name = "int-div";
+    name = "nonlin-int-mult";
     codomain = Type.INT;
-    domain = [Type.INT;Type.INT];
+    domain = [Type.INT; Type.INT];
     is_argument_valid = (function
-                         | [x ; y] -> x =/= y
-                                   && (x =/= Const (Value.Int 0)) && (x =/= Const (Value.Int 1))
+                         | [x ; y] -> (x =/= Const (Value.Int 0)) && (x =/= Const (Value.Int 1))
                                    && (y =/= Const (Value.Int 0)) && (y =/= Const (Value.Int 1))
                          | _ -> false);
-    evaluate = (function [@warning "-8"]
-                | [Value.Int x ; Value.Int y] when y <> 0 -> Value.Int (pos_div x y));
-    to_string = (fun [@warning "-8"] [a ; b] -> "(div " ^ a ^ " " ^ b ^ ")");
-    global_constraints = (fun [@warning "-8"] [_ ; b] -> ["(not (= 0 " ^ b ^ "))"]);
+    evaluate = (function [@warning "-8"] [Value.Int x ; Value.Int y] -> Value.Int (x * y));
+    to_string = (fun [@warning "-8"] [a ; b] -> "(* " ^ a ^ " " ^ b ^ ")");
+    global_constraints = (fun _ -> [])
   } ;
   {
     name = "int-mod";
@@ -32,17 +30,5 @@ let components = let (=/=) = (fun x y -> (not (Expr.equal x y))) in [
                 | [Value.Int x ; Value.Int y] when y <> 0 -> Value.Int (x % y));
     to_string = (fun [@warning "-8"] [a ; b] -> "(mod " ^ a ^ " " ^ b ^ ")");
     global_constraints = (fun [@warning "-8"] [_ ; b] -> ["(not (= 0 " ^ b ^ "))"]);
-  } ;
-  {
-    name = "nonlin-int-mult";
-    codomain = Type.INT;
-    domain = [Type.INT; Type.INT];
-    is_argument_valid = (function
-                         | [x ; y] -> (x =/= Const (Value.Int 0)) && (x =/= Const (Value.Int 1))
-                                   && (y =/= Const (Value.Int 0)) && (y =/= Const (Value.Int 1))
-                         | _ -> false);
-    evaluate = (function [@warning "-8"] [Value.Int x ; Value.Int y] -> Value.Int (x * y));
-    to_string = (fun [@warning "-8"] [a ; b] -> "(* " ^ a ^ " " ^ b ^ ")");
-    global_constraints = (fun _ -> [])
   }
 ]
