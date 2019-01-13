d6cf7609df866d5adfe6d1c3f8b0a8a5ab562730
Saswat Padhi <padhi@cs.ucla.edu>
2019-01-13 22:16:28 +0000

    Invariant generation only using exhaustive enumeration

--- src/LIG.ml
+++ src/LIG.ml
@@ -24,78 +24,79 @@ let default_config = {
   model_completion_mode = `RandomGeneration ;
 }
 
-let satisfyTrans ?(conf = default_config) ~(sygus : SyGuS.t) ~(z3 : ZProc.t)
-                 ~(states : Value.t list list) (inv : Job.desc)
-                 : (Job.desc * ZProc.model option) =
-  Stats.add_candidate () ;
+let learnInvariant_internal ?(conf = default_config) (sygus : SyGuS.t)
+                            ~(states : Value.t list list) (z3 : ZProc.t)
+                            (seed_string : string): Job.desc =
   let invf_call =
-       "(invf " ^ (List.to_string_map sygus.inv_func.args ~sep:" " ~f:fst) ^ ")" in
+      "(invf " ^ (List.to_string_map sygus.inv_func.args ~sep:" " ~f:fst) ^ ")" in
   let invf'_call =
     "(invf " ^ (List.to_string_map sygus.inv_func.args ~sep:" "
                   ~f:(fun (s, _) -> s ^ "!")) ^ ")" in
   let eval_term = (if not (conf.model_completion_mode = `UsingZ3) then "true"
                    else "(and " ^ invf_call ^ " " ^ sygus.trans_func.expr ^ ")") in
-  let rec helper inv =
-  begin
-    Log.info (lazy ("IND >> Strengthening for inductiveness:"
-                   ^ (Log.indented_sep 4) ^ inv)) ;
-    if String.equal inv "false" then ("false", None) else
-    let inv_def = "(define-fun invf ("
-                ^ (List.to_string_map sygus.inv_func.args ~sep:" "
-                                      ~f:(fun (s, t) -> "(" ^ s ^ " " ^ (Type.to_string t) ^ ")"))
-                ^ ") Bool " ^ inv ^ ")"
-    in ZProc.create_scope z3 ~db:[ inv_def ; "(assert " ^ sygus.trans_func.expr ^ ")"
-                                           ; "(assert " ^ invf_call ^ ")" ]
-     ; let pre_inv =
-         VPIE.learnVPreCond
-           ~conf:conf.for_VPIE ~consts:sygus.constants ~eval_term ~post_desc:invf'_call ~z3
-           (Job.create_positive states
-              ~f:(ZProc.constraint_sat_function ("(not " ^ invf'_call ^ ")")
-                    ~z3 ~arg_names:(List.map sygus.synth_variables ~f:fst))
-              ~args: sygus.synth_variables
-              ~post: (fun _ res -> match res with
-                                   | Ok v when v = Value.Bool false -> true
-                                   | _ -> false))
-      in ZProc.close_scope z3
-       ; Log.debug (lazy ("IND Delta: " ^ pre_inv))
-       ; if String.equal pre_inv "true" then ((Stats.add_candidate ()) ; (inv, None))
-         else begin
-           let new_inv = "(and " ^ pre_inv ^ " " ^ inv ^ ")"
-            in Log.info (lazy ("PRE >> Checking if the following candidate is weaker than precond:"
-                        ^ (Log.indented_sep 4) ^ new_inv)) ;
-               let ce = (ZProc.implication_counter_example z3 sygus.pre_func.expr new_inv)
-               in if ce = None then helper new_inv else ((Stats.add_candidate ()) ; (new_inv, ce))
-         end
-  end in helper inv
-
-let rec learnInvariant_internal ?(conf = default_config) (restarts_left : int)
-                                ~(states : Value.t list list) (sygus : SyGuS.t)
-                                (seed_string : string) (z3 : ZProc.t) : Job.desc =
-  let open Quickcheck in
-  let open Simulator in
-  let restart_with_new_states head =
-    if restarts_left < 1
-    then (Log.error (lazy ("Reached MAX (" ^ (string_of_int conf.max_restarts)
-                          ^ ") restarts. Giving up ..."))
-         ; "false")
-    else begin
-      Log.warn (lazy ("Restarting inference engine. Attempt "
-                     ^ (string_of_int (1 + conf.max_restarts - restarts_left))
-                     ^ "/" ^ (string_of_int conf.max_restarts) ^".")) ;
-      learnInvariant_internal
-        ~states:List.(dedup_and_sort ~compare:(compare Value.compare) (
-          states @ (random_value ~size:conf.max_steps_on_restart ~seed:(`Deterministic seed_string)
-                                 (simulate_from sygus z3 head))))
-        ~conf (restarts_left - 1) sygus (seed_string ^ "#") z3
-    end
-  in match satisfyTrans ~conf ~sygus ~states ~z3 (sygus.post_func.expr) with
-     | inv, None
-       -> if inv <> "false" then ZProc.simplify z3 inv
-          else restart_with_new_states (random_value ~seed:(`Deterministic seed_string)
-                                                     (gen_pre_state ~use_trans:true sygus z3))
-     | _, (Some ce_model)
-       -> restart_with_new_states (random_value ~seed:(`Deterministic seed_string)
-                                                (gen_state_from_model sygus (Some ce_model)))
+  let rec helper good_states bad_states =
+    Log.debug (lazy ("Invoking synthesizer with "
+                      ^ (conf.for_VPIE.for_PIE.synth_logic.name) ^ " logic."
+                      ^ (Log.indented_sep 0) ^ "Conflict group ("
+                      ^ (List.to_string_map sygus.synth_variables ~sep:" , "
+                           ~f:(fun (v,t) -> v ^ " :" ^ (Type.to_string t))) ^ "):" ^ (Log.indented_sep 2)
+          ^ "POS (" ^ (Int.to_string (List.length good_states)) ^ "):" ^ (Log.indented_sep 4)
+                      ^ (List.to_string_map good_states ~sep:(Log.indented_sep 4)
+                           ~f:(fun vl -> "(" ^ (List.to_string_map vl ~f:Value.to_string ~sep:" , ") ^ ")")) ^ (Log.indented_sep 2)
+          ^ "NEG (" ^ (Int.to_string (List.length bad_states)) ^ "):" ^ (Log.indented_sep 4)
+                      ^ (List.to_string_map bad_states ~sep:(Log.indented_sep 4)
+                           ~f:(fun vl -> "(" ^ (List.to_string_map vl ~f:Value.to_string ~sep:" , ") ^ ")")))) ;
+    let open Synthesizer in
+    let result = solve sygus.constants {
+      logic = conf.for_VPIE.for_PIE.synth_logic ;
+      arg_names = List.map sygus.synth_variables ~f:fst ;
+      inputs = (
+        let all_inputs = good_states @ bad_states
+         in List.mapi sygus.synth_variables
+                      ~f:(fun i _ -> Array.of_list List.(
+                                       map all_inputs ~f:(fun l -> nth_exn l i)))) ;
+      outputs = Array.of_list ( (List.map good_states ~f:(fun _ -> Value.Bool true))
+                              @ (List.map bad_states ~f:(fun _ -> Value.Bool false)))
+    } in
+    let candidate = if result.constraints = [] then result.string
+                    else ( "(and " ^ result.string
+                         ^ (String.concat ~sep:" " result.constraints)
+                         ^ ")")
+     in (Stats.add_candidate ())
+      ; if candidate = "false" then "false" else
+     let open ZProc in
+     let open Quickcheck in
+     let inv_def = "(define-fun invf ("
+                 ^ (List.to_string_map sygus.inv_func.args ~sep:" "
+                                       ~f:(fun (s, t) -> "(" ^ s ^ " " ^ (Type.to_string t) ^ ")"))
+                 ^ ") Bool " ^ candidate ^ ")"
+      in Log.info (lazy ("GUESS >> Candidate invariant:"
+                   ^ (Log.indented_sep 4) ^ candidate))
+       ; match implication_counter_example z3 candidate sygus.post_func.expr with
+          | Some ce
+            -> let [@warning "-8"] Some state =
+                  random_value ~seed:(`Deterministic seed_string)
+                                (Simulator.gen_state_from_model sygus (Some ce))
+                in helper good_states (state :: bad_states)
+          | None -> begin match implication_counter_example z3 sygus.post_func.expr candidate with
+                      | Some ce
+                        -> let [@warning "-8"] Some state =
+                              random_value ~seed:(`Deterministic seed_string)
+                                            (Simulator.gen_state_from_model sygus (Some ce))
+                            in helper (state :: good_states) bad_states
+                      | None -> ZProc.create_scope z3 ~db:[ inv_def ; "(assert " ^ sygus.trans_func.expr ^ ")" ]
+                              ; let ind_ce = implication_counter_example ~eval_term z3 invf_call invf'_call
+                                in close_scope z3
+                                  ; begin match ind_ce with
+                                      | Some ce
+                                        -> let [@warning "-8"] Some state =
+                                              random_value ~seed:(`Deterministic seed_string)
+                                                            Simulator.(gen_state_from_model sygus (Some (filter_state ~trans:true ce)))
+                                            in helper (state :: good_states) bad_states
+                                      | None -> candidate
+                                    end
+                    end
+   in helper states []
 
 let learnInvariant ?(conf = default_config) ~(states : Value.t list list)
                    ~(zpath : string) (sygus : SyGuS.t) : Job.desc =
@@ -105,4 +106,4 @@ let learnInvariant ?(conf = default_config) ~(states : Value.t list list)
                                                                    (Generator.small_non_negative_int)))))
        (fun z3 -> Simulator.setup sygus z3
                 ; if not ((implication_counter_example z3 sygus.pre_func.expr sygus.post_func.expr) = None) then "false"
-                  else learnInvariant_internal ~conf ~states conf.max_restarts sygus conf.base_random_seed z3)
+                  else learnInvariant_internal ~conf ~states sygus z3 conf.base_random_seed)
